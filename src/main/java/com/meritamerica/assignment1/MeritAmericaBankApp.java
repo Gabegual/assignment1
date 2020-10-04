49008f22918c2c895170790abee12c2bc5559f20
Gabegual <Roblox4@gmail.com>
2020-10-04 13:11:06 -0400

    MeritAmericaBank.App

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,14 @@
 package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
+	public static void main(String [] args) {
+		AccountHolder MAB = new AccountHolder("Sadiq", "", "Fish", "560-43-0743", 100.00, 1000.00);
+      System.out.println(MAB.toString());
+      MAB.getCheckingAccount().deposit(500);
+      MAB.getSavingsAccount().withdraw(800);
+      System.out.println(MAB.getCheckingAccount().toString());
+      System.out.println(MAB.getSavingsAccount().toString());
+      
+	}
 	
 }
